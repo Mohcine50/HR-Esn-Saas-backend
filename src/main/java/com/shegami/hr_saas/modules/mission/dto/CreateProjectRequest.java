487f99d1d2d5c710e288c2dc86cdf8dbfa926fa0
Mohcine lahnin <mohcinela1998@gmail.com>
2026-02-22 23:42:27 +0000

    Add project necessary dtos

--- src/main/java/com/shegami/hr_saas/modules/mission/dto/CreateProjectRequest.java
+++ src/main/java/com/shegami/hr_saas/modules/mission/dto/CreateProjectRequest.java
@@ -1,4 +1,22 @@
 package com.shegami.hr_saas.modules.mission.dto;
 
+import com.shegami.hr_saas.modules.mission.enums.Priority;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class CreateProjectRequest {
+
+    @NotBlank(message = "Project name is required")
+    private String name;
+
+    private String description;
+
+    @NotNull(message = "Priority is required")
+    private Priority priority;
+
+    private Set<String> tags;
 }
--- src/main/java/com/shegami/hr_saas/modules/mission/dto/UpdateProjectRequest.java
+++ src/main/java/com/shegami/hr_saas/modules/mission/dto/UpdateProjectRequest.java
@@ -1,4 +1,15 @@
 package com.shegami.hr_saas.modules.mission.dto;
 
+import com.shegami.hr_saas.modules.mission.enums.Priority;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
 public class UpdateProjectRequest {
+
+    private String name;
+    private String description;
+    private Priority priority;
+    private Set<String> tags;
 }
